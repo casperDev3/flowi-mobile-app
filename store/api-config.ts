dc48ef3ce743bb35f3639ce38615b9ed6fa3c802
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2026-05-15 00:19:26 +0300

    Update api-config.ts

--- store/api-config.ts
+++ store/api-config.ts
@@ -1,15 +1,2 @@
-const ENV = __DEV__ ? 'local' : 'production';
-
-const config = {
-  local: {
-    API_BASE: 'http://localhost:8000/api',
-    WS_BASE: 'ws://localhost:8000/ws',
-  },
-  production: {
-    API_BASE: 'https://api.flowi.casperdev.site/api',
-    WS_BASE: 'wss://api.flowi.casperdev.site/ws',
-  },
-};
-
-export const API_BASE = config[ENV].API_BASE;
-export const WS_BASE = config[ENV].WS_BASE;
+export const API_BASE = 'https://api.flowi.casperdev.site/api';
+export const WS_BASE = 'wss://api.flowi.casperdev.site/ws';
